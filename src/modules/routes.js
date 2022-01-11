3971035ab0f20592d79d02e289092fcdaf18c0c8
CariSeniorProgrammer <95581231+CariSeniorProgrammer@users.noreply.github.com>
2022-01-11 17:32:13 +0100

    CHANGE: linting again

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -2,16 +2,14 @@ import express from "express";
 import path from "path";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "../../swagger_output.json";
-import logger from "./logger";
 import setupAppforAuthentication from "./privateRoutes";
 import setupPromclient from "./middleware/promClient";
 
 
 export default (app) => {
-
   setupAppforAuthentication(app);
 
-  
+
   app.use("/api-docs", (req, res, next) => {
     // #swagger.ignore = true
     next();
@@ -26,7 +24,7 @@ export default (app) => {
   });
 
   // error route
-  
+
   app.get("/error", (req, res, next) => {
     // #swagger.ignore = true
     next(error);
@@ -34,7 +32,7 @@ export default (app) => {
 
   app.get("/metrics", setupPromclient.register);
   // 404 route
-  
+
   app.get("*", (req, res, next)=> {
     // #swagger.ignore = true
     res.status(404).send("We couldn't find this page");
