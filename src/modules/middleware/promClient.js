062532462fd9d459a7272c32d55fd0f0b482dfc3
CariSeniorProgrammer <95581231+CariSeniorProgrammer@users.noreply.github.com>
2022-01-13 14:38:04 +0100

    ADD: add metrics for client IPs

--- src/modules/middleware/promClient.js
+++ src/modules/middleware/promClient.js
@@ -15,6 +15,12 @@ const httpRequests = new promClient.Counter({
   labelNames: ["requests"],
 });
 
+const httpRequestsIps = new promClient.Counter({
+  name: "http_requests_ips",
+  help: "Count client IPs",
+  labelNames: ["IP"],
+});
+
 const countRequests = new promClient.Counter({
   name: "http_resource_counts",
   help: "Count specific calls",
@@ -29,9 +35,13 @@ export default {
         });
   },
   thisOneCounts: (req, res, next) => {
-    if (!["/metrics", "/favicon.ico"].includes(req.url) ) {
+    if (!["/metrics", "/favicon.ico"].includes(req.url)) {
+      const ip = req.headers["x-forwarded-for"] ||
+        req.socket.remoteAddress ||
+        null;
       httpRequests.inc();
       countRequests.labels(req.url).inc();
+      httpRequestsIps.labels(ip).inc();
     }
 
     next();
