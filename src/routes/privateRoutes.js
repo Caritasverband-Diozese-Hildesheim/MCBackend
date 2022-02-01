65293f9fa2214330c45562795685d79266cbbfea
CariSeniorProgrammer <bjoern.meier@caritas-dicvhildesheim.de>
2022-02-01 14:57:18 +0100

    FIX: optimize login

--- src/routes/privateRoutes.js
+++ src/routes/privateRoutes.js
@@ -1,7 +1,5 @@
 import session from "express-session";
 import Keycloak from "keycloak-connect";
-import passport from "passport";
-import {Issuer, Strategy} from "openid-client";
 import configuration from "../modules/configuration";
 import VPERoutes from "./VSP/router/mydocuments";
 import confluenceSite from "../modules/externalAPIs/confluenceSite";
@@ -25,9 +23,8 @@ const keycloak = new Keycloak({store: memoryStore}, kcConfig);
  * @function setupPrivateRoutes
  * @description Extra function so that swagger-autogen recognizes the routes
  * @param {ExpressApp} app  The App Object to configure for authentification
- * @param {ExpressApp} client  The authentification-client
  */
-const setupPrivateRoutes = (app, client) => {
+const setupPrivateRoutes = (app) => {
   app.use(
       session({
         secret: "oidc:meincaritaskc.azurewebsites.net",
@@ -38,59 +35,11 @@ const setupPrivateRoutes = (app, client) => {
   );
 
   app.use(keycloak.middleware());
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  passport.use(
-      "oidc",
-      new Strategy({client}, (tokenSet, userinfo, done) => {
-        return done(null, tokenSet.claims());
-      }),
-  );
-
-  // handles serialization and deserialization of authenticated user
-  passport.serializeUser((user, done) => {
-    done(null, user);
-  });
-  passport.deserializeUser((user, done) => {
-    done(null, user);
-  });
-  // start logout request
-  app.get("/logout", (req, res) => {
-    // #swagger.ignore = true
-    res.redirect(client.endSessionUrl());
-  });
 
 
-  // logout callback
-  app.get("/logout/callback", (req, res) => {
-    // #swagger.ignore = true
-    // clears the persisted user from the local storage
-    req.logout();
-    // redirects the user to a public route
-    res.redirect("/");
-  });
-  // start authentication request
-  app.get("/auth", (req, res, next) => {
-    // #swagger.ignore = true
-    passport.authenticate("oidc")(req, res, next);
-  });
-  app.get("/login", (req, res, next) => {
-    // #swagger.ignore = true
-    passport.authenticate("oidc")(req, res, next);
-  });
-
-  // authentication callback
-  app.get("/auth/callback", (req, res, next) => {
-    // #swagger.ignore = true
-    passport.authenticate("oidc", {
-      successRedirect: "/me",
-      failureRedirect: "/",
-    })(req, res, next);
-  });
   app.get("/me", keycloak.protect(), (req, res) => {
     // #swagger.ignore = true
-    res.status(200).send(JSON.stringify(req.user));
+    res.status(200).send(JSON.stringify(req.kauth.grant.access_token.content));
   });
 
   app.use("/VSP", keycloak.enforcer(["res_vsp:sc_view"]), VPERoutes);
@@ -127,17 +76,6 @@ const setupPrivateRoutes = (app, client) => {
  * @param {ExpressApp} app  The App Object to configure for authentification
  */
 export default (app) => {
-  Issuer.discover(`${configuration.OIDCAuthUrl}/realms/${configuration.OIDCRealm}`)
-      .then((criiptoIssuer) => {
-        const client = new criiptoIssuer.Client({
-        /* eslint-disable camelcase */
-          client_id: configuration.OIDCClientId,
-          client_secret: configuration.OIDCSecretToken,
-          redirect_uris: [configuration.OIDCRedirectUrlCallback],
-          post_logout_redirect_uris: [configuration.OIDCRedirectUrlLogout],
-          token_endpoint_auth_method: "client_secret_post",
-        });
-        setupPrivateRoutes(app, client);
-      });
+        setupPrivateRoutes(app);    
 };
 
